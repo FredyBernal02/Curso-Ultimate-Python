75a89ccd448e2975b2478c701795fc7e1b8e2a7f
FredyBernal02 <fredybernal0224@gmail.com>
2025-05-07 22:08:34 -0400

    Se incluyeron varios ejercicios mas sobre tipos de datos

--- tipos/01-variables.py
+++ tipos/01-variables.py
@@ -57,16 +57,111 @@
 # print("El tipo de resultado es:", type(resultado))
 
 # Ejercicio: Haz un programa que pida la edad y muestre el doble
-while True:
-    entrada = input(
-        "¿Cuantos años tienes? (o escribe 'salir' para terminar): ").lower()
+# while True:
+#     entrada = input(
+#         "¿Cuantos años tienes? (o escribe 'salir' para terminar): ").lower()
 
-    if entrada == "salir":
-        break  # rompe el bucle
+#     if entrada == "salir":
+#         break  # rompe el bucle
 
-    try:
-        edad = int(entrada)
-        doble = edad * 2
-        print(f"El doble de tus {edad} años es: {doble}")
-    except ValueError:
-        print("Por favor, ingresa un número entero valido.")
+#     try:
+#         edad = int(entrada)
+#         doble = edad * 2
+#         print(f"El doble de tus {edad} años es: {doble}")
+#     except ValueError:
+#         print("Por favor, ingresa un número entero valido.")
+
+#  Reto 1: Tu nombre y edad
+# Crea un programa que:
+
+# Pida al usuario que escriba su nombre y su edad usando input().
+
+# Guarde esos valores en variables.
+
+# Muestre un mensaje que diga:
+# "Hola [nombre], tienes [edad] años."
+
+# nombre = input("¿Cual es tu nombre?: ")
+# edad = int(input("¿Cuantos años tienes?: "))
+
+# print(f"Hola {nombre}, tienes {edad} años")
+# print(f"El tipo de 'nombre' es: {type(nombre)}")
+# print(f"El tipo de 'edad' es: {type(edad)}")
+
+# 🧮 Reto 3: Calculadora de suma
+# Crea un programa que:
+
+# Pida al usuario dos números (pueden ser decimales).
+
+# Convierta ambos a float.
+
+# Sume los dos números.
+
+# Muestre el resultado de la suma con un mensaje como:
+# "La suma de [número1] + [número2] es: [resultado]"
+
+
+# n1 = float(input("Escribe el primer numero: "))
+# n2 = float(input("Escribe el segundo numero: "))
+
+# resultado = n1 + n2
+
+# print(f"La suma de {n1} y {n2} es: {resultado}")
+
+# ➗ Reto 4: Calculadora completa
+# Ahora, amplia tu calculadora para que haga más operaciones:
+
+# Pide dos números(como antes).
+
+# Convierte los números a float.
+
+# Realiza las siguientes operaciones:
+
+# Suma
+
+# Resta
+
+# Multiplicación
+
+# División(asegurándote de no dividir por 0)
+
+# Muestra los resultados de cada operación de manera clara y ordenada.
+
+# numero1 = float(input("Escribe el primer numero: "))
+# numero2 = float(input("Escribe el segundo numero: "))
+
+# suma = numero1 + numero2
+# resta = numero1 - numero2
+# multiplicacion = numero1 * numero2
+
+# print(f"La suma entre {numero1} y {numero2} es: {suma}")
+# print(f"La resta entre {numero1} y {numero2} es: {resta}")
+# print(f"La multiplicacion entre {numero1} y {numero2} es: {multiplicacion}")
+
+# if numero2 != 0:
+#     division = numero1 / numero2
+#     print(f"La division entre {numero1} y {numero2} es: {division}")
+# else:
+#     print("No se puede dividir por cero.")
+
+# Reto 5: ¿Mayor de edad?
+# Crea un programa que:
+
+# Pida al usuario su edad.
+
+# Convierte la entrada a int.
+
+# Verifique si el usuario es mayor o menor de edad(mayor o igual a 18 años).
+
+# Muestra un mensaje como:
+
+# "Eres mayor de edad"
+
+# "Eres menor de edad"
+
+edad = int(input("Escribe tu edad: "))
+
+if edad <= 18:
+    print("Eres menor de edad")
+else:
+    print("Eres mayor de edad")
