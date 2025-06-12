37f9322bb5cd48b7274a3f1fc08cb02f95f22056
FredyBernal02 <fredybernal0224@gmail.com>
2025-06-12 17:55:32 -0400

    Realice practicas con las listas

--- tipos-avanzados/02-manipulando-listas.py
+++ tipos-avanzados/02-manipulando-listas.py
@@ -1,11 +1,16 @@
-mascotas = ["Wolfgang", "Pelusa", "Pulga", "Copito"]
-print(mascotas[0])
-mascotas[0] = "Bicho"
-# print(mascotas)
-# print(mascotas[2:])
-# print(mascotas[-1])
+# mascotas = ["Wolfgang", "Pelusa", "Pulga", "Copito"]
+# print(mascotas[0])
+# mascotas[0] = "Bicho"
+# # print(mascotas)
+# # print(mascotas[2:])
+# # print(mascotas[-1])
 # print(mascotas[1:2:2])
 
-numeros = list(range(21))
-print(numeros[::2])
-print(numeros[1::2])
+# numeros = list(range(21))
+# print(numeros[::2])
+# print(numeros[1::2])
+
+curso = ["Fredy", "Johanna", "Tatiana", "Dania"]
+curso[3] = "Daniel"
+curso[0] = "Lucas"
+print(curso[2::])
